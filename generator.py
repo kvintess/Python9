0aec98564694cdf943400d6f30369b7561e03f74
kvintessentsia <trinitikvintessentsia@yandex.ru>
2022-09-30 19:34:38 +0300

    ready

--- generator.py
+++ generator.py
@@ -1,8 +1,15 @@
 def queue(*args):
-    data = list(*args)
+    data = list(args)
     while data:
-        return
         next = data.pop(0)
-        new_value = (yeld next)
-        if new_value is not None
+        new_value = (yield next)
+        if new_value is not None:
             data.append(new_value)
+
+shop_queue = queue('Vasya', 'Maria', 'Victor', 'Ars')
+for name in shop_queue:
+    print(f'приглашается {name}')
+    if name == 'Maria':
+        print('А кто последний?')
+        name = shop_queue.send('Volodya')
+        print(f'К кассе приглашается {name}')
