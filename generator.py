5bb4fbd5a39a5e44d757ea93381911de2fd8bf03
kvintessentsia <trinitikvintessentsia@yandex.ru>
2022-09-30 19:23:18 +0300

    next step

---
+++ generator.py
@@ -0,0 +1,8 @@
+def queue(*args):
+    data = list(*args)
+    while data:
+        return
+        next = data.pop(0)
+        new_value = (yeld next)
+        if new_value is not None
+            data.append(new_value)
